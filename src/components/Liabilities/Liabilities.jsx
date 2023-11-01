30096fbf670b9bf3594286e6d3551162b0d6971c
Lucas Martin <driftpvp@gmail.com>
2023-11-01 16:07:19 -0500

    react components

--- src/components/Liabilities/Liabilities.jsx
+++ src/components/Liabilities/Liabilities.jsx
@@ -1,21 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import {useSelector, useDispatch} from 'react-redux';
 import './Liabilities.css';
 
-// Basic functional component structure for React with default state
-// value setup. When making a new component be sure to replace the
-// component name TemplateFunction with the name for the new component.
 function Liabilities() {
-  // Using hooks we're creating local state for a "heading" variable with
-  // a default value of 'Functional Component'
   const dispatch = useDispatch();
   const liabilitiesList = useSelector((store) => store.liabilitiesList)
-  // const store = useSelector((store) => store);
   const [liabilities_name, setLiabilitiesName] = useState('');
-
-  // const [liabilitiesList, setLiabilitiesList] = useState([]);
-  // const [liablilitiesName, setLiabilitiesName] = useState ('');
   const [liabilities_note, setLiabilitiesNote] = useState ('');
   const [liabilities_value, setLiabilitiesValue] = useState (0);
 
@@ -26,15 +16,6 @@ function Liabilities() {
   const getLiabilitiesList = () => {
     dispatch({ type: 'FETCH_LIABILITIES' })
   };
-
-  // const fetchLiabilities = () => {
-  //   axios.get('/api/liabilities').then((response) => {
-  //       setLiabilitiesList(response.data);
-  //   }).catch((error) => {
-  //       console.log(error);
-  //       alert('Something went wrong with Liabilities GET');
-  //   });
-  // }
 
   return (
     <div>
